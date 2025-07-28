f50c1b6514e84c0bbcebe9307c9f1543e83eaa06
camiicode <dftp93@gmail.com>
2025-07-28 14:48:28 -0500

    [2707202505] Config tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: '15px',
+      screens: {
+        DEFAULT: '100%',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1440px'
+      }
+    }
+  }
+}
